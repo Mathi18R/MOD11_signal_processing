ca8d9921f9c5e41979c07c571491fd253d21b1af
Mathi18R <m.b.c.reus@student.utwente.nl>
2023-03-17 14:53:00 +0100

    Created cross_correlation.h

---
+++ MOD_11_signal_processing/header/cross_correlation.h
@@ -0,0 +1,21 @@
+#include "buffer.h"
+
+#pragma once
+
+template<typename T, int SIZE>
+T cross_correlation_sub(const buffer<T, SIZE>& bufferA, const buffer<T, SIZE>& bufferB, int offset=0) {
+	T result = T(0);
+
+	for (int i = 0; i < SIZE; i++) {
+		result += bufferA[i] * bufferB[i + offset];
+	}
+
+	return result;
+}
+
+template<typename T, int SIZE>
+void cross_correlation(const buffer<T, SIZE>& bufferA, const buffer<T, SIZE>& bufferB, T (&result)[(2 * SIZE) - 1]) {
+	for (int i = -SIZE +1 ; i < SIZE; i++) {
+		result[SIZE - i - 1] = cross_correlation_sub(bufferA, bufferB, i);
+	}
+}
