8cb5577695832767411ab1f3a27fb5ae3e7246e6
Mathi18R <m.b.c.reus@student.utwente.nl>
2023-04-14 15:11:17 +0200

    Sequential 4 mics

--- MOD_11_signal_processing/src/angle_handler.h
+++ MOD_11_signal_processing/src/angle_handler.h
@@ -16,12 +16,30 @@ int get_maximum(T (&array)[SIZE]){
 }
 
 template<typename T, int SIZE>
-void angle_handler(){
-    float tau_LF_RF;
+int angle_handler(){
+    float tau_LF_RF = 0;
+    float tau_LF_LB = 0;
+    float tau_LF_RB = 0;
+	float tau_RF_LB = 0;
+    float tau_RF_RB = 0;
+    float tau_LB_RB = 0;
 
-    cross_correlation<float, SIZE, sample_swing>(bufferLF, bufferRF, cross_correlation_buffer);
-    tau_LF_RF = float(get_maximum(cross_correlation_buffer) - SIZE) / Fs_mic;
+    cross_correlation(bufferLF, bufferRF, cross_correlation_buffer1);
+    tau_LF_RF = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
+    cross_correlation(bufferLB, bufferRB, cross_correlation_buffer2);
+    tau_LB_RB = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
 
-    Serial.print("       tau_LF_RF      ");
-    Serial.print(tau_LF_RF);
+    cross_correlation(bufferLF, bufferLB, cross_correlation_buffer1);
+    tau_LF_LB = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
+    cross_correlation(bufferRF, bufferRB, cross_correlation_buffer2);
+    tau_RF_RB = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
+
+    cross_correlation(bufferLF, bufferRB, cross_correlation_buffer1);
+    tau_LF_RB = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
+    cross_correlation(bufferRF, bufferLB, cross_correlation_buffer2);
+    tau_RF_LB = float(get_maximum(cross_correlation_buffer) - sample_swing) / Fs_mic;
+
+
+    region = my_least_mean_square.find_region(tau_LF_RF, tau_LF_LB, tau_LF_RB, tau_RF_LB, tau_RF_RB, tau_LB_RB);
+
 }
--- MOD_11_signal_processing/src/main.cpp
+++ MOD_11_signal_processing/src/main.cpp
@@ -11,13 +11,20 @@ const int SIZE = 4000;
 
 buffer<float, SIZE> bufferLF;
 buffer<float, SIZE> bufferRF;
+buffer<float, SIZE> bufferLB;
+buffer<float, SIZE> bufferRB;
 
 const float Fs_mic = 20000;
 const float Fs_output = 1000;
-const int sample_swing = 20000*0.001;//Change this when changing Fs_mic
-float cross_correlation_buffer[2*sample_swing-1];
 
-int region = 0;
+const int sample_swing = 20000*0.001;//Change this when changing Fs_mic
+float cross_correlation_buffer1[2*sample_swing-1];
+float cross_correlation_buffer2[2*sample_swing-1];
+
+int region = 0
+
+least_mean_square my_least_mean_square({ -0.1, 0.1 }, { 0.1, 0.1 }, { 0.1, -0.1 }, { -0.1, -0.1 });
+    
 
 //Needs to be included after mutex and buffer declarations
 #include "input_handler.h"
