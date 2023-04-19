57e5204d44c0acdd6a2657714c34f2bcc3041dcb
Mathi18R <m.b.c.reus@student.utwente.nl>
2023-04-19 12:37:24 +0200

    changed moving_average.h

--- MOD_11_signal_processing/src/moving_average.h
+++ MOD_11_signal_processing/src/moving_average.h
@@ -1,8 +1,34 @@
 #pragma once
-class moving_average
+class moving_average_factor_threshold
 {
 public:
-	moving_average(double Threshold, double Input_weight) {
+	moving_average_factor_threshold(double Threshold_factor, double Input_weight) {
+		average = 0;
+		threshold_factor = Threshold_factor;
+		input_weight = Input_weight;
+	}
+
+	bool insert(double input) {
+		double averageNew = (1 - input_weight) * average + input_weight * input;
+		bool threshold_reached = false;
+		if (averageNew > threshold_factor*average) {
+			threshold_reached = true;
+		}
+		average = averageNew;
+		return threshold_reached;
+	}
+
+
+private:
+	double threshold_factor;
+	double input_weight;
+	double average;
+};
+
+class moving_average_constant_threshold
+{
+public:
+	moving_average_constant_threshold(double Threshold, double Input_weight) {
 		average = 0;
 		threshold = Threshold;
 		input_weight = Input_weight;
@@ -10,12 +36,38 @@ public:
 
 	bool insert(double input) {
 		double averageNew = (1 - input_weight) * average + input_weight * input;
-		if (averageNew - average > threshold) {
-			average = averageNew;
-			return true;
+		bool threshold_reached = false;
+		if (averageNew > threshold) {
+			threshold_reached = true;
 		}
 		average = averageNew;
-		return false;
+		return threshold_reached;
+	}
+
+
+private:
+	double threshold;
+	double input_weight;
+	double average;
+};
+
+class moving_average_difference_threshold
+{
+public:
+	moving_average_difference_threshold(double Threshold, double Input_weight) {
+		average = 0;
+		threshold = Threshold;
+		input_weight = Input_weight;
+	}
+
+	bool insert(double input) {
+		double averageNew = (1 - input_weight) * average + input_weight * input;
+		bool threshold_reached = false;
+		if (averageNew - average > threshold) {
+			threshold_reached = true;
+		}
+		average = averageNew;
+		return threshold_reached;
 	}
 
 
